03b890a1e312fc082695074df605a5ca0831dc92
Rick Grundy <rick@thoughtworks.com>
2011-05-05 15:30:03 -0700

    Empty Rails 3.0.4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChunksCMSCom::Application.config.secret_token = '5c9e028398cbbe5c1f74504227cdc2c780e8112a81824b28bc868c72b0fede8d26283f9ab95092e0281eb0aaa0ce715e00ac26986268999a6ccc3dd8ceb8987e'
